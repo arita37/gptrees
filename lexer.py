c40a07035c9271160735a1141495b2cb5027849e
boriel <boriel@2c8c9c0e-2861-11df-b6db-4758a1b4a51e>
2010-03-05 14:41:04 +0000

    Primera importacion.
    git-svn-id: http://gptrees.googlecode.com/svn/trunk@2 2c8c9c0e-2861-11df-b6db-4758a1b4a51e

---
+++ lexer.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ply.lex as lex
+
+TOKENS = ('ID', 
+    'LP', # Left parenthesis
+    'RP', # Right parenthesis
+    'STR', # A literal string, e.g. '+'
+    'COMMA', #
+    'IS', # Derivation ::= or -->
+)
+
+
+
+class Lexer(object):
+    ''' Lexer embedded in a class so several Lexer instances
+    can be used in the same program.
+    '''
+    def __init__(self, tokens = TOKENS):
+        ''' Constructor: tokens = t-uple with TOKENS list
+        '''
+        self.lex = None
+        self.tokens = tokens
+
+    def t_skip(self, t):
+        r'[ \t\n]+' # Matches one or more spaces and tabs
+        pass      # and skips them
+
+    def t_error(self, t):
+        ''' Error handling rule. Called on lexical error.
+        '''
+        print "illegal character '%s'" % t.value[0]
+
+    # --- TOKENS patterns --- #
+
+    def t_LP(self, t):
+        r'\('
+        return t
+
+    def t_RP(self, t):
+        r'\)'
+        return t
+
+    def t_ID(self, t):
+        r'[a-zA-Z][a-zA-Z\d]*'
+        return t
+
+    def t_COMMA(self, t):
+        r','
+        return t
+
+    def t_IS(self, t):
+        r'-->|::='
+        return t
+
+    # ---
+
+    def input(self, str):
+        ''' Reads str as input for lexical analysis.
+        '''
+        self.input_data = str
+        self.lex = lex.lex(object = self) # Creates a lexer instance
+        self.lex.input = self.input_data  # Fills internal lexer with str chars.
+
+    def token(self):
+        ''' Returns next token to the parser
+        '''
+        return self.lex.token()
+
+
+# Creates a dummy instance needed to create lexer states
+tmp = lex.lex(object = Lexer(), lextab = 'lextab')
+
+if __name__ == '__main__':
+    import sys
+    
+    tmp.input(sys.stdin.read())
+    print "aa"
+    while True:
+        token = tmp.token()
+        if not token:
+            break
+        print token
+
+
