c7581942b5da6e1ccd8d67f1516d2a72e93eb8b9
boriel <boriel@2c8c9c0e-2861-11df-b6db-4758a1b4a51e>
2010-03-08 23:43:46 +0000

    Some fixes, so this already works.
    git-svn-id: http://gptrees.googlecode.com/svn/trunk@16 2c8c9c0e-2861-11df-b6db-4758a1b4a51e

--- parser.py
+++ parser.py
@@ -13,32 +13,44 @@ import symbol
 def p_S(p):
     ''' S : TreeDefinition
     '''
+    p[0] = [p[1]]
+    print p[0]
 
 def p_Slist(p):
     ''' S : S TreeDefinition
     '''
+    p[0] = p[1] + [p[2]]
+    print p[0]
 
 def p_tree_is_terminal(p):
     ''' TreeDefinition : ID IS Terminal
     '''
+    p[0] = symbol.TreeDefinition(symbol.ID(p[1]), p[3])
+    print p[0]
 
 def p_tree_is_subtree(p):
     ''' TreeDefinition : ID IS Terminal LP Arglist RP
     '''
+    p[0] = symbol.TreeDefinition(symbol.ID(p[1]), p[3], p[5])
+    print p[0]
 
 def p_arglist_is_tree(p):
     ''' Arglist : Terminal
     '''
+    p[0] = symbol.Arglist(p[1])
+    print p[0]
 
 def p_arglist_is_list_arglist(p):
     ''' Arglist : Arglist COMMA Terminal
     '''
+    p[0] = symbol.Arglist(p[1], p[3])
+    print p[0]
 
 def p_terminal_is_ID(p):
     ''' Terminal : ID
     '''
     p[0] = symbol.ID(p[1])
-    print p[0].symbol_name
+    print p[0]
 
 def p_terminal_is_STR(p):
     ''' Terminal : STRING
--- symbol/treedefinition.py
+++ symbol/treedefinition.py
@@ -5,9 +5,10 @@
 from symbol import Symbol
 
 class TreeDefinition(Symbol):
-    def __init__(self, terminal, arglist = None):
-        Symbol.__init__()
-        self.terminal = terminal
-        self.arglist = arglist
+    def __init__(self, name, terminal, arglist = None):
+        Symbol.__init__(self)
+        self.name = name # Tree symbol (left side of the rule)
+        self.terminal = terminal # 1st symbol in the right side
+        self.arglist = arglist # Optional arglist
 
 
