c5468fe57964373cb6ac5d8a60b29dca8ffcf1a9
boriel <boriel@2c8c9c0e-2861-11df-b6db-4758a1b4a51e>
2010-03-14 13:03:46 +0000

    The duplicated examples has been changed to match the new grammar definition.
    git-svn-id: http://gptrees.googlecode.com/svn/trunk@41 2c8c9c0e-2861-11df-b6db-4758a1b4a51e

--- examples/duplicated.s
+++ examples/duplicated.s
@@ -12,16 +12,19 @@ def new_number():
 
 %}
 
-
 TREE --> BINARY;
 TREE --> UNARY;
-UNARY --> NUM {new_numner() };
+UNARY --> NUM {new_number() };
 UNARY --> VAR;
 VAR --> 'x';
 
 BINARY --> '+'(TREE, TREE)
-         | '-'(TREE, TREE)
-         | '*'(TREE, NUM { new_number() })
+         | '-'(TREE, TREE) 
+         | '*'(TREE, NUM)
          | '/'(TREE, TREE)
 ;
+
+# The following rule will cause an error,
+# since NUM has already a defined generator in line #17
+TMP --> NUM { new_number2() };
 
